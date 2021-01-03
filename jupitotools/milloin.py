2027d2a8cfc14e5cd38d0c6167cc6e6793a434db
Jussi Toivonen <jupito@iki.fi>
2021-01-03 08:43:54 +0200

    cli

--- jupitotools/milloin.py
+++ jupitotools/milloin.py
@@ -84,19 +84,6 @@ def yield_filepaths(paths, glob='*.milloin'):
             yield path
 
 
-def print_dates(dates, today):
-    for date in dates:
-        marker = '*' if date == today else ' '
-        print(f'{marker} {date}')
-
-
-def print_events(events):
-    for event in events:
-        print(event)
-        if any([event.time(), event.location()]):
-            print('-', event.time(), event.location())
-
-
 def date_marker(date, today):
     """Return a one-character date marker."""
     if date == today:
@@ -108,8 +95,49 @@ def date_marker(date, today):
     return ' '
 
 
-def print_agenda(matches, today):
+@click.group()
+@click.option('-P', '--path', type=Path, help='Input path')
+@click.option('-t', '--today', help='Date today.')
+@click.option('-f', '--future', type=int, default=7,
+              help='Number of future days to show (current day included).')
+@click.option('-p', '--past', type=int, default=1,
+              help='Number of past days to show.')
+@click.option('--fmt', default='date_week', help='Output date format.')
+@click.option('-v', '--verbose', count=True, help='Increase verbosity.')
+@click.pass_context
+def cli(ctx, path, today, future, past, fmt, verbose):
+    """A when(1)-style calendar application."""
+    ctx.ensure_object(dict)
+    paths = [path]
+    if not paths:
+        # ~/.config/milloin/*.milloin
+        paths = [Path(click.get_app_dir('milloin'))]
+    paths = list(yield_filepaths(paths))
+    today = Date.fromisoformat(today) if today else Date.today()
+    dates = [today.tomorrow(x) for x in range(-past, future + 1)]
+    events = [Event(y, x) for x in paths for y in valid_lines(x)]
+    ctx.obj['paths'] = paths
+    ctx.obj['today'] = today
+    ctx.obj['dates'] = dates
+    ctx.obj['events'] = events
+    ctx.obj['fmt'] = DEFAULT_FORMATS.get(fmt, fmt)
+    if verbose > 0:
+        print(f'Today is {today}.')
+        print(f'Filepaths: {", ".join(str(x) for x in paths)}.')
+
+
+@cli.command()
+@click.pass_context
+def agenda(ctx):
     """Print agenda."""
+    today = ctx.obj['today']
+
+    def collect_matches(date):
+        def event_matches(event):
+            return event.match(date, today)
+        return filter(event_matches, ctx.obj['events'])
+    matches = {x: list(collect_matches(x)) for x in ctx.obj['dates']}
+
     for date, events in matches.items():
         marker = date_marker(date, today)
         datestr_normal = str(date)
@@ -122,59 +150,20 @@ def print_agenda(matches, today):
             print(f'{marker} {datestr} {eventstr}')
 
 
-@click.group()
-@click.option('-t', '--today', help='Date today.')
-@click.option('-f', '--future', type=int, default=7,
-              help='Number of future days to show (current day included).')
-@click.option('-p', '--past', type=int, default=1,
-              help='Number of past days to show.')
-@click.option('--fmt', default='date_week', help='Output date format.')
-@click.option('-v', '--verbose', count=True, help='Increase verbosity.')
+@cli.command()
 @click.pass_context
-def cli(ctx, today, future, past, fmt, verbose):
-    """A when(1)-style calendar application."""
-    ctx.ensure_object(dict)
-    today = Date.fromisoformat(today) if today else Date.today()
-    ctx.obj['today'] = today
-    ctx.obj['dates'] = [today.tomorrow(x) for x in range(-past, future + 1)]
-    ctx.obj['fmt'] = DEFAULT_FORMATS.get(fmt, fmt)
+def dates(ctx):
+    """List dates."""
+    for date in ctx.obj['dates']:
+        marker = date_marker(date, ctx.obj['today'])
+        print(f'{marker} {date}')
 
 
 @cli.command()
-@click.argument('paths', nargs=-1, type=Path, metavar='[PATH]...')
 @click.pass_context
-def agenda(ctx, paths):
-    """Show agenda."""
-    if not paths:
-        # ~/.config/milloin/*.milloin
-        paths = [Path(click.get_app_dir('milloin'))]
-    verbose = ctx.parent.params['verbose']
-    today = ctx.obj['today']
-    dates = ctx.obj['dates']
-
-    # for each file:
-    #     for each line:
-    #         parse, create, collect entry
-    # make date objects
-    # for each date:
-    #     for each entry:
-    #         if expression evaluates true with date, show description
-
-    paths = list(yield_filepaths(paths))
-    events = [Event(y, x) for x in paths for y in valid_lines(x)]
-
-    def collect_matches(date, events, today):
-        # it = (x if x.match(date, today) else None for x in events)
-        # yield from filter(None, it)
-        def event_matches(event):
-            return event.match(date, today)
-        return filter(event_matches, events)
-    matches = {x: list(collect_matches(x, events, today)) for x in dates}
-
-    if verbose > 0:
-        print(f'Today is {today}.')
-        print(f'Filepaths: {", ".join(str(x) for x in paths)}.')
-        print_dates(dates, today)
-    if verbose > 1:
-        print_events(events)
-    print_agenda(matches, today)
+def events(ctx):
+    """List all events."""
+    for event in ctx.obj['events']:
+        print(event)
+        if any([event.time(), event.location()]):
+            print('-', event.time(), event.location())
