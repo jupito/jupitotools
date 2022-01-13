c3f63a7839dad5084372809392536964fe786db4
Jussi Toivonen <jupito@iki.fi>
2022-01-13 20:31:01 +0200

    force reinstall

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -ex
 python3 setup.py sdist bdist_wheel
-pip3 install --user -U dist/jupitotools-0.0.1-py3-none-any.whl
+pip3 install --user -U --force-reinstall dist/jupitotools-0.0.1-py3-none-any.whl
