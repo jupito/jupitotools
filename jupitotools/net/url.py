706e5ca6a9413f0d97a8e67a3aa0f0bb26c1dac0
Jussi Toivonen <jupito@iki.fi>
2022-01-30 23:11:23 +0200

    misc

--- jupitotools/net/url.py
+++ jupitotools/net/url.py
@@ -14,31 +14,30 @@ http://psyvault.net/viewtopic.php?f=8&t=476&sid=578a367c6b421ddbc6c4263c2e5d2a81
 http://tajunta.net/blog/tuukka-virtaperko-ja-laadun-metafysiikka/#.X7BS5_r8K00
 '''
 
+CHAFF_QUERIES = 'fbclid,shared,utm_medium,utm_source'.split(',')
+CHAFF_FRAGMENT_PREFIXES = ['Echobox=']
 
-def remove_tracking_chaff(url):
+
+def remove_chaff(url):
     """Remove some known tracking chaff."""
-    queries = 'fbclid,shared,utm_medium,utm_source'.split(',')
-    fragment_prefixes = ['Echobox=']
-    for query in queries:
+    for query in CHAFF_QUERIES:
         if query in url.qp:
             del url.qp[query]
-    if any(url.fragment.startswith(x) for x in fragment_prefixes):
+    if any(url.fragment.startswith(x) for x in CHAFF_FRAGMENT_PREFIXES):
         url.fragment = ''
 
 
 @click.command()
 @click.option('-n', '--normalize', is_flag=True, help='Normalize')
 @click.option('-f', '--full_quote', is_flag=True, help='Full quote')
-@click.option('-T', '--remove_tracking', is_flag=True,
-              help='Remove some known tracking chaff')
-# @click.option('-f', '--future', type=int, default=7,
-def cli_scan_urls(normalize, full_quote, remove_tracking):
+@click.option('-c', '--nochaff', is_flag=True, help='Remove tracking chaff')
+def cli_scan_urls(normalize, full_quote, nochaff):
     """Scan input stream for URLs."""
     urls = chain(*(boltons.urlutils.find_all_links(x) for x in sys.stdin))
     for url in urls:
         if normalize:
             url.normalize()
-        if remove_tracking:
-            remove_tracking_chaff(url)
+        if nochaff:
+            remove_chaff(url)
         text = url.to_text(full_quote=full_quote)
         print(text)
